6e32544b460f21338a82ff50ce58b15caf05006d
Misha Afanasiuk <mafanasiuk@keepersecurity.com>
2025-09-04 11:46:09 -0500

    Updated tests

--- test/proxy_test.go
+++ test/proxy_test.go
@@ -7,24 +7,6 @@ import (
 	ksm "github.com/keeper-security/secrets-manager-go/core"
 )
 
-func TestProxyUrlEnvVar(t *testing.T) {
-	// Save and defer restore of KSM_PROXY
-	origProxy := os.Getenv("KSM_PROXY")
-	defer os.Setenv("KSM_PROXY", origProxy)
-
-	testUrl := "http://localhost:8888"
-	os.Setenv("KSM_PROXY", testUrl)
-
-	configJson := MockConfig{}.MakeJson(MockConfig{}.MakeConfig(nil, "", "", ""))
-	config := ksm.NewMemoryKeyValueStorage(configJson)
-
-	sm := ksm.NewSecretsManager(&ksm.ClientOptions{Config: config})
-
-	if sm.ProxyUrl != testUrl {
-		t.Errorf("Expected ProxyUrl to be %s from KSM_PROXY env var, got %s", testUrl, sm.ProxyUrl)
-	}
-}
-
 func TestProxyUrlArgument(t *testing.T) {
 	proxyUrl := "http://myproxy:9999"
 
@@ -38,34 +20,7 @@ func TestProxyUrlArgument(t *testing.T) {
 	}
 }
 
-func TestProxyUrlArgumentOverridesEnvVar(t *testing.T) {
-	// Save and defer restore of KSM_PROXY
-	origProxy := os.Getenv("KSM_PROXY")
-	defer os.Setenv("KSM_PROXY", origProxy)
-
-	envProxy := "http://envproxy:8080"
-	argProxy := "http://argproxy:9090"
-
-	os.Setenv("KSM_PROXY", envProxy)
-
-	configJson := MockConfig{}.MakeJson(MockConfig{}.MakeConfig(nil, "", "", ""))
-	config := ksm.NewMemoryKeyValueStorage(configJson)
-
-	sm := ksm.NewSecretsManager(&ksm.ClientOptions{Config: config, ProxyUrl: argProxy})
-
-	if sm.ProxyUrl != argProxy {
-		t.Errorf("Expected ProxyUrl to be %s (argument should override env), got %s", argProxy, sm.ProxyUrl)
-	}
-}
-
 func TestProxyUrlDefaultEmpty(t *testing.T) {
-	// Save and defer restore of KSM_PROXY
-	origProxy := os.Getenv("KSM_PROXY")
-	defer os.Setenv("KSM_PROXY", origProxy)
-
-	// Unset KSM_PROXY
-	os.Unsetenv("KSM_PROXY")
-
 	configJson := MockConfig{}.MakeJson(MockConfig{}.MakeConfig(nil, "", "", ""))
 	config := ksm.NewMemoryKeyValueStorage(configJson)
 
